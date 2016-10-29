3a00a9da0ea17876beeb2e7780d98966ff0e4a3d
Yury Prokashev <yury.prokashev@gmail.com>
2016-10-29 16:50:24 +0300

    mongo prod ip

--- src/PayloadModel.js
+++ src/PayloadModel.js
@@ -4,8 +4,8 @@
 
 var mongoose = require('mongoose');
 
-const MONGO_HOST = "mongodb://localhost:27017/pfin";
-// const MONGO_HOST = "mongodb://54.229.108.38:27017/pfin";
+// const MONGO_HOST = "mongodb://localhost:27017/pfin";
+const MONGO_HOST = "mongodb://54.229.108.38:27017/pfin";
 
 mongoose.connect(MONGO_HOST);
 
